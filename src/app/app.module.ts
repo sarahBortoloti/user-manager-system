d9c011bd26cd84e656eb4ef1b07e7445414425b3
Sarah Garzon <sarah.bortoloti.15@gmail.com>
2020-02-09 13:21:42 -0300

    [UMS-01] setup projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { TableComponent } from './core/components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
